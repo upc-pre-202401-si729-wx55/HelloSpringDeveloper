ff775c1daa5a85850a627a588c901cb369c7b19d
Angel Velasquez <pcisavel@upc.edu.pe>
2024-03-27 12:26:13 -0500

    chore: initial commit.

---
+++ src/main/java/com/acme/hellospringdeveloper/HelloSpringDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.acme.hellospringdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringDeveloperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/hellospringdeveloper/HelloSpringDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.hellospringdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
